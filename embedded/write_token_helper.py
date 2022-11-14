ae2b07331bfc3680340d36954ff293a948db2869
Paul Schwind <paul.schwind@tum.de>
2022-11-14 19:10:27 +0100

    add writing helper script

---
+++ embedded/write_token_helper.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+writer = SimpleMFRC522()
+
+import json
+
+# just for testing ourselves
+
+try:
+    writer.write(json.dumps({
+        "id": input("Card ID (C12345): ") or "C12345",
+        "card_token": input("Card token (1234567890): ") or "1234567890",
+    }))
+except KeyboardInterrupt :
+    GPIO.cleanup()
+    raise
