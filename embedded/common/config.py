dc9515cef4b4d6ca4f58139bd93f0f1ab40c10b4
Paul Schwind <paul.schwind@tum.de>
2022-11-20 15:24:48 +0100

    add delivery service

---
+++ embedded/common/config.py
@@ -0,0 +1,13 @@
+import json
+
+
+class Config:
+    def __init__(self, config_file_name: str):
+        with open(config_file_name) as f:
+            config = json.load(f)
+        self.box_id = config["box_id"]
+        self.box_name = config["box_name"]
+        self.street_address = config["street_address"]
+        self.auth_server_address = config["auth_server_address"]
+        self.delivery_server_address = config["delivery_server_address"]
+        self.delivery_server_access_token = config["delivery_server_access_token"]
--- embedded/main.py
+++ embedded/main.py
@@ -1,7 +1,12 @@
+from api.delivery_service import DeliveryService
 try:
     from ase_io.hardware import ase_in, ase_out, ase_mainloop
 except ImportError:
     from ase_io.software import ase_in, ase_out, ase_mainloop
+from common.config import Config
+
+config = Config("config.json")
+delivery_service = DeliveryService(config)
 
 
 if __name__ == '__main__':
