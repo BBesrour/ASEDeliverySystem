7abd4946c08974ede06dc4e3269c5c75d424faf8
pal03377 <pal03377@users.noreply.github.com>
2022-12-06 13:15:58 +0100

    add pin test setup

---
+++ embedded/pin_test_setup.py
@@ -0,0 +1,19 @@
+# one pin on high, one pin on low, one input pin
+
+import RPi.GPIO as GPIO
+import time
+
+HIGH_PIN = 16
+LOW_PIN = 26
+INPUT_PIN = 6
+
+# set up LEDs
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(HIGH_PIN, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(LOW_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(INPUT_PIN, GPIO.IN)
+
+while True:
+    time.sleep(1)
+    print("Input:", GPIO.input(INPUT_PIN))
