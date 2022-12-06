d5ce2acecfd099eede999c05bb099ce2027da8a9
pal03377 <pal03377@users.noreply.github.com>
2022-12-06 13:53:47 +0100

    input to led

--- embedded/pin_test_setup.py
+++ embedded/pin_test_setup.py
@@ -6,6 +6,7 @@ import time
 HIGH_PIN = 16
 LOW_PIN = 26
 INPUT_PIN = 6
+LED_PIN = 17
 
 # set up LEDs
 GPIO.setmode(GPIO.BCM)
@@ -13,11 +14,14 @@ GPIO.setwarnings(False)
 GPIO.setup(HIGH_PIN, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(LOW_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(INPUT_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 try:
     while True:
         time.sleep(.1)
         print("Input:", GPIO.input(INPUT_PIN))
+        GPIO.output(LED_PIN, GPIO.input(INPUT_PIN))
+
 except KeyboardInterrupt:
     GPIO.cleanup()
     print("Cleaned up.")
