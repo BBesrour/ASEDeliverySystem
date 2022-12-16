c5cb2130955a580ca63eb4086a15ba7b0ee47ae0
Mohamed Bilel Besrour <ge49gej@mytum.de>
2022-12-16 17:11:25 +0100

    Fix DiscoveryServerApplication package

--- microservices/discovery-server/src/main/java/com/group40/discoveryserver/DiscoveryServerApplication.java
+++ microservices/discovery-server/src/main/java/com/group40/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.group40.discoveryserver;
+package com.group40.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
