1371d0cd0f7425892417f8535e13e750117b690b
Paul Schwind <paul.schwind@tum.de>
2023-01-21 12:36:25 +0100

    fix role singular in frontend

--- frontend/src/storage/user.ts
+++ frontend/src/storage/user.ts
@@ -11,14 +11,12 @@ export function removeAccessToken() {
 }
 
 
-export function getRoles(): string[] {
-    return JSON.parse(localStorage.getItem('roles') || '[]');
+export function getRole(): string {
+    return localStorage.getItem('role') ?? "";
 }
 
-export function setRoles(roles: string[]) {
-    if (roles) {
-        localStorage.setItem('roles', JSON.stringify(roles));
-    }
+export function setRole(role: string) {
+    localStorage.setItem('role', role);
 }
 
 export function getUserID(): string {
