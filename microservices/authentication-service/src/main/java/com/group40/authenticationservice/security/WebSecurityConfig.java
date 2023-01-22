d4740f16feeaaa971da621bcbbbc27febfacc1eb
Paul Schwind <paul.schwind@tum.de>
2023-01-22 15:14:22 +0100

    remove CORS disable-ing

--- microservices/authentication-service/src/main/java/com/group40/authenticationservice/security/WebSecurityConfig.java
+++ microservices/authentication-service/src/main/java/com/group40/authenticationservice/security/WebSecurityConfig.java
@@ -1,25 +1,22 @@
 package com.group40.authenticationservice.security;
 
+import com.group40.authenticationservice.security.jwt.AuthEntryPointJwt;
+import com.group40.authenticationservice.security.jwt.AuthTokenFilter;
+import com.group40.authenticationservice.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.group40.authenticationservice.security.jwt.AuthEntryPointJwt;
-import com.group40.authenticationservice.security.jwt.AuthTokenFilter;
-import com.group40.authenticationservice.security.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -69,22 +66,10 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.cors().and().csrf().disable()
-//			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-//			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-//			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-//			.antMatchers("/api/test/**").permitAll()
-//			.anyRequest().authenticated();
-//
-//		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//	}
 	
 	@Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable()
+    http
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authorizeRequests().antMatchers("/api/auth/**").permitAll()
