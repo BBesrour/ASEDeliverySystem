8e56c86b94111e567b48846e74592ed52621cdf3
Paul Schwind <paul.schwind@tum.de>
2023-01-22 17:47:13 +0100

    add red green test script

---
+++ embedded/test_outputs.py
@@ -0,0 +1,24 @@
+import asyncio
+
+try:
+    from ase_io.hardware import ase_in, ase_out, ase_mainloop
+except ImportError:
+    from ase_io.software import ase_in, ase_out, ase_mainloop
+from threading import Timer
+
+
+async def on_off():
+    while True:
+        print("Green!")
+        ase_out.light_green()
+        await asyncio.sleep(1)
+        ase_out.turn_off()
+        print("Red!")
+        ase_out.light_red()
+        await asyncio.sleep(1)
+        ase_out.turn_off()
+
+if __name__ == '__main__':
+    thread = Timer(1, lambda: asyncio.run(on_off()))
+    thread.start()
+    ase_mainloop()
