b419896297718d159765e8d6a861a1d94fc40b5a
Kevin Nguyen <kevin.nguyen@tum.de>
2023-01-23 18:19:30 +0100

    fix delivery not found error

--- frontend/src/components/pages/deliveries/UpdateDeliveryDialog.tsx
+++ frontend/src/components/pages/deliveries/UpdateDeliveryDialog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
@@ -7,41 +7,38 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Delivery from "../../../model/Delivery";
 import {updateDelivery} from "../../../api/delivery/deliveries";
 import DeliveryFields from "./DeliveryFields";
+import {DialogContent} from "@mui/material";
 
-export default function UpdateDeliveryDialog({open, currentDelivery, handleClose, onDeliveryUpdated}: {
-    open: boolean;
-    currentDelivery: Delivery,
-    handleClose: () => void;
-    onDeliveryUpdated: (delivery: Delivery) => void;
+export default function UpdateDeliveryDialog({open, handleClose, onDeliveryUpdated, delivery}: {
+    open: boolean,
+    handleClose: () => void,
+    onDeliveryUpdated: (delivery: Delivery) => void,
+    delivery: Delivery
 }) {
-    const [delivery, setDelivery] = React.useState(new Delivery(
-        currentDelivery.id,
-        currentDelivery.targetCustomerID,
-        currentDelivery.targetBoxID,
-        currentDelivery.delivererID,
-        currentDelivery.status,
-        currentDelivery.isActive,
-    ));
-    async function handleUpdateDelivery() {
-        await updateDelivery(delivery);
-        onDeliveryUpdated(delivery);
+    const [changedDelivery, setChangedDelivery] = useState(delivery);
+
+    async function updateDeliveryAndClose() {
+        await updateDelivery(changedDelivery);
+        onDeliveryUpdated(changedDelivery);
         handleClose();
     }
 
-    return open ? (
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Update User</DialogTitle>
-            <DialogContentText>
-                Enter new delivery details here:
-            </DialogContentText>
-            <DeliveryFields delivery={delivery} onDeliveryUpdate={setDelivery} />
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleUpdateDelivery}>Update</Button>
-            </DialogActions>
-        </Dialog>
-    ) : (
-        <></>
+    return (
+        open ?
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Update Delivery</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Enter new delivery details here:
+                    </DialogContentText>
+                    <DeliveryFields delivery={delivery} onDeliveryUpdate={setChangedDelivery}/>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={updateDeliveryAndClose}>Update</Button>
+                </DialogActions>
+            </Dialog>
+            : <></>
     );
 }
 
