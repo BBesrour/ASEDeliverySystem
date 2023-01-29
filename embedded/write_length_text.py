fbfebb7b5435842b4fffb9661234c31897707adc
Paul Schwind <paul.schwind@tum.de>
2023-01-29 15:10:47 +0100

    read text fix

--- embedded/write_length_text.py
+++ embedded/write_length_text.py
@@ -8,7 +8,7 @@ for i in range(1, 100):
     try:
         mfrc.write("x" * i)
         # verify
-        text = mfrc.read()
+        _, text = mfrc.read()
         print("Text", text)
         if text != "x" * i:
             print("Failed to write length", i)
