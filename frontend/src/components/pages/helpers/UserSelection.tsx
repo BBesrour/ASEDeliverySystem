c4d0a1aa246aef6ee90926953cb345ba957b010a
ufukyagmur <uyagmur123@gmail.com>
2023-01-29 13:48:44 +0100

    a

--- frontend/src/components/pages/helpers/UserSelection.tsx
+++ frontend/src/components/pages/helpers/UserSelection.tsx
@@ -1,44 +1,74 @@
-import React, {useEffect, useState} from "react";
-import {Autocomplete} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Autocomplete } from "@mui/material";
 import TextField from "@mui/material/TextField";
-import User, {UserRole} from "../../../model/User";
-import {getAllUsers} from "../../../api/delivery/user";
+import User, { UserRole } from "../../../model/User";
+import { getAllUsers } from "../../../api/delivery/user";
 
 interface AutocompleteDelivererOption {
-    delivererID: string;
-    label: string;
+  userID: string;
+  label: string;
 }
 
-export default function UserSelection({label, onSelect, userRole}: { label: string, onSelect: (user: User | null) => void, userRole: UserRole }) {
-    const [users, setUsers] = useState<User[]>([]);
-    useEffect(() => {
-        getAllUsers().then((users) => {
-            setUsers(users);
-        });
-    }, []);
+export default function UserSelection({
+  label,
+  onSelect,
+  userRole,
+  userId,
+}: {
+  label: string;
+  onSelect: (user: User | null) => void;
+  userRole: UserRole;
+  userId: string;
+}) {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    getAllUsers().then((users) => {
+      setUsers(users);
+    });
+  }, []);
 
-    const [userOptions, setUserOptions] = useState<AutocompleteDelivererOption[]>([]);
-    useEffect(() => {
-        setUserOptions(users.filter((user) => user.role === userRole).map((user) => {
-            return {
-                delivererID: user.id || "",
-                label: user.name
-            }
-        }));
-    }, [users]);
+  const [userOptions, setUserOptions] = useState<AutocompleteDelivererOption[]>(
+    []
+  );
+  useEffect(() => {
+    setUserOptions(
+      users
+        .filter((user) => user.role === userRole)
+        .map((user) => {
+          return {
+            userID: user.id || "",
+            label: user.email,
+          };
+        })
+    );
+  }, [users]);
 
-    function getUserByID(UserID: string): User | null {
-        return users.find((user) => user.id === UserID) || null;
-    }
+  function getUserByID(UserID: string): User | null {
+    return users.find((user) => user.id === UserID) || null;
+  }
 
-    return <Autocomplete
-        disablePortal
-        options={userOptions}
-        getOptionLabel={(user) => user.delivererID}
-        sx={{width: 300}}
-        renderInput={(params) => <TextField {...params} label={label}/>}
-        onChange={(event, newValue) => {
-            onSelect(getUserByID(newValue?.delivererID || ""));
-        }}
-    />;
+  console.log(getUserByID(userId)?.email);
+  return (
+    <Autocomplete
+      disablePortal
+      options={userOptions}
+      isOptionEqualToValue={(option, value) => option.userID === value.userID}
+      defaultValue={{ userID: userId, label: getUserByID(userId)?.email || "" }}
+      getOptionLabel={(user) => {
+        console.log(user);
+        return user.label;
+      }}
+      sx={{ width: 300 }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={label}
+          defaultValue={getUserByID(userId)?.email || ""}
+        />
+      )}
+      onChange={(event, newValue) => {
+        onSelect(getUserByID(newValue?.userID || ""));
+      }}
+    />
+  );
 }
